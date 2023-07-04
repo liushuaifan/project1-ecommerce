d3dd6a47659e3294de6404e555a8dfc15140796f
lingyunlirobin <lilingyu@usc.edu>
2023-07-03 17:10:28 -0700

    add products content; add product content

--- ecommerceapp/src/components/Product.js
+++ ecommerceapp/src/components/Product.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+import Button from '@mui/material/Button';
+import './Product.css';
 
-function Product() {
+function Product({product}) {
   return (
-    <div>Product</div>
+    <div>
+      <Card >
+        <img className="productImage"src={product.img} alt="Apple Watch" />
+        <CardContent>
+          <div>
+            <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant='body2' color='textSecondary' />
+            <Typography variant="h5">
+                {product.price}
+            </Typography>
+          </div>
+        </CardContent>
+        <div className="buttonContainer">
+          <Button variant="contained" fullWidth>Add</Button>
+          <Button variant="outlined" fullWidth>Edit</Button>
+        </div>
+
+      </Card>
+    </div>
+   
   )
 }
 
--- ecommerceapp/src/components/Products.js
+++ ecommerceapp/src/components/Products.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Product from './Product';
+import appleWatch from '../image/appleWatch.jpg'
+import './Products.css';
+
+const products = [
+  { id:1 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:2 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:3 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:4 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:5 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:6 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:7 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:8 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:9 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+  { id:10 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:appleWatch},
+]
 
 function Products() {
   return (
-    <div>Products</div>
+    <main>
+      <div className="productGrid">
+        {products.map((product)=>(
+            <div className="productGridItem" key={product.id}>
+                <Product product={product}/>
+            </div>
+        ))}
+      </div>    
+    </main>
   )
 }
 
