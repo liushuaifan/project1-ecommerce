77f8e733b053267e5e9874d908843fd7344de8ca
lingyunlirobin <lilingyu@usc.edu>
2023-07-06 17:30:30 -0700

    fix bug in ProductDetail component

--- ecommerceapp/src/components/ProductDetail.js
+++ ecommerceapp/src/components/ProductDetail.js
@@ -7,25 +7,25 @@ import './style/ProductDetail.css';
 
 function ProductDetail() {
 
-  const { ProductID } = useParams();
+  const { ProductId } = useParams();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
+    
     const products = [
-      { id:1 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:2 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:3 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:1 , name: 'Apple Watch', price: '$199.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:2 , name: 'Apple Watch', price: '$299.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:3 , name: 'Apple Watch', price: '$399.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
       { id:4 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:5 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:6 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:7 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:8 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:9 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
-      { id:10 , name: 'Apple Watch', price: '$499.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:5 , name: 'Apple Watch', price: '$599.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:6 , name: 'Apple Watch', price: '$699.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:7 , name: 'Apple Watch', price: '$799.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:8 , name: 'Apple Watch', price: '$899.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:9 , name: 'Apple Watch', price: '$999.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
+      { id:10 , name: 'Apple Watch', price: '$1099.00', description: 'Apple Watch, 126G', img:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/SmartSelect_20180913_104037_Firefox.jpg'},
     ]
-    console.log(ProductID)
-    setProduct(products.find(product => product.id === Number('1')))
-  }, [ProductID]);
+    setProduct(products.find(product => product.id === Number(ProductId)))
+  }, [ProductId]);
 
   return (
     <div className='productDetail'>
