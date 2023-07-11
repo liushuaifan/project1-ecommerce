0393c7e45e565d0022fc73d99253f4abb3225f65
liushuaifan <f.liushuai@wustl.edu>
2023-07-11 17:49:15 -0500

    finish add cart front end, try set up cart server

--- ecommerce-server/handlers/cart.js
+++ ecommerce-server/handlers/cart.js
@@ -1,1 +1,59 @@
-// get, post, put
+// createCart,
+// getCart,
+// updateCart
+
+// get, post, put, delete
+const db = require('../models');
+
+// POST - /api/users/:id/products
+exports.createCart = async function (req, res, next) {
+  try {
+    console.log(db.Product)
+
+    const product = await db.Product.create({
+      cartValue: req.body.cartValue,
+      email: req.body.email,
+      imageurl: req.body.imageurl,
+      price: req.body.price,
+      productname: req.params.productname
+    });
+
+    // find the user by id
+    // const foundUser = await db.User.findById(req.params.id);
+    // push the product id to the user's messages array
+    // foundUser.products.push(product.id);
+    // save the user
+    // await foundUser.save();
+    // send back the product with the user id
+    const foundProduct= await db.Product.findById(product._id).populate(
+      'user',
+      {
+        username: true
+      }
+    );
+    return res.status(200).json(foundProduct);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// GET - /api/users/:id/products/:product_id
+exports.getCart = async function (req, res, next) {
+  try {
+    const product = await db.Product.findById(req.params.product_id);
+    return res.status(200).json(product);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// PUT - /api/users/:id/products/:product_id
+exports.updateCart = async function (req, res, next) {
+  try {
+    const product = await db.Product.findByIdAndUpdate(req.params.product_id, req.body);
+    await product.save();
+    return res.status(200).json(product);
+  } catch (err) {
+    return next(err);
+  }
+};
