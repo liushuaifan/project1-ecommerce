29e71efe32411518c47ca5978cc8fe7bd4e76adf
liushuaifan <f.liushuai@wustl.edu>
2023-07-14 10:37:14 -0500

    finish cart add and delete

--- ecommerceapp/src/components/Cart.js
+++ ecommerceapp/src/components/Cart.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { useState, useLayoutEffect} from 'react';
+import { useState, useLayoutEffect, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { fetchUserCart } from '../services/cart';
+import { fetchUserCart, createCart , updateCart, fetchCart } from '../services/cart';
+
 
 import "./style/cart.css";
 
@@ -15,7 +17,7 @@ const style = {
   left: '50%',
   transform: 'translate(70%, -60%)',
   width: 500,
-  height:600,
+  height:700,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -24,11 +26,16 @@ const style = {
 
 export default function BasicModal() {
   const [open, setOpen] = React.useState(true);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const navigate = useNavigate();
 
+  const handleClose = (e) => {
+    e.preventDefault();
+    navigate("/")
+  }
   const [products, setProducts] = useState([]);
   const { user } = useSelector(state => state.user);
+
+
 
   useLayoutEffect(() => {
 
@@ -50,14 +57,70 @@ export default function BasicModal() {
   }, [user.id]);
 
 
+  const AddtoCart = (product) => {
+      // console.log(product.cartValue +  product.productid   + "clicked")
+      const data = {
+        cartValue:1+product.cartValue,
+        email:localStorage.getItem("email"),
+        imageurl:product.imageurl,
+        price:product.price,
+        productname:product.productname,
+        productId:product.productid,
+        userId: user.id
+      };
+      // console.log("hi")
+      // console.log(data);
+      updateCart(data);
+
+      let index = 0;
+      // console.log(product.productid);
+      // console.log(("hello"));
+      for(let i=0;i<products.length;i++){
+        // console.log(products[i]);
+        if(products[i].productid===product.productid) index = i;
+      }
+
+      const mycopy = [...products];
+      mycopy[index].cartValue ++;
+      setProducts(mycopy);
+  }
+
+  const MinusCart = (product) =>{
+      if(product.cartValue === 1){
+        //delete cart
+          
+      }else{
+
+      const data = {
+        cartValue:product.cartValue-1,
+        email:localStorage.getItem("email"),
+        imageurl:product.imageurl,
+        price:product.price,
+        productname:product.productname,
+        productId:product.productid,
+        userId: user.id
+      };
+
+      updateCart(data);
+      let index = 0;
+      for(let i=0;i<products.length;i++){
+
+        if(products[i].productid===product.productid) index = i;
+      }
+
+      const mycopy = [...products];
+      mycopy[index].cartValue --;
+      setProducts(mycopy);
+  }
+}
+
+
   return (
     <div>
       
       <Modal
         open={open}
-        onClose={open}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+
       >
         <Box sx={style}>
           <div>
@@ -73,11 +136,16 @@ export default function BasicModal() {
           {products.length > 0 && products.map((product, index) => (
             <div key={index} style={{width:"200px" , height:"80px"}}>
               <img src={product.imageurl} alt="pend" style={{width: "70px",height:"70px", position:"relative",top:"20px"}}/>
-              <h2 className='cartName' style={{position:"relative",top:"-78px",left:"35%"}}>{product.productname}</h2>
-              <h3 className='cartPrice' style={{position:"relative",top:"-118px",left:"190%"}}>{product.price}</h3>
-              <Button style={{backgroundColor: 'yellow',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-118px",left:"35%"}} >-</Button>
-              <Button style={{backgroundColor: 'yellow',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-118px",left:"150%"}} >Remove</Button>
-              <Button style={{backgroundColor: 'yellow',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-118px",left:"10%"}} >+</Button>
+              <h2 className='cartName' style={{color:"black", position:"relative",top:"-78px",left:"65%"}}>{product.productname}</h2>
+              <h3 className='cartPrice' style={{color:"blue",position:"relative",top:"-118px",left:"190%"}}>{product.price}</h3>
+              
+              
+             
+              <Button onClick={()=> AddtoCart(product)} style={{backgroundColor: 'yellow',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-118px",left:"35%"}} >+</Button>
+              <h5 style={{position:"relative",top:"-178px",left:"75%",  fontSize: "20px", color:"black"}}>{product.cartValue} </h5>
+              <Button onClick={()=> MinusCart(product)} style={{backgroundColor: 'yellow',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-238px",left:"95%"}} >-</Button>
+              <Button style={{backgroundColor: 'white',borderColor: 'red',width:"10px",height:"30px", position:"relative",top:"-238px",left:"150%"}} >Remove</Button>
+
             </div>
           ))}
 
@@ -98,10 +166,10 @@ export default function BasicModal() {
             <button type="submit" className='discountbuton' style={{position:"relative", top:"10px",left:"70%", color:"white"}}>apply discount  </button>
         </div>
         
-        <h style={{position:"absolute", top:"60%"}}> subtotal </h>
-        <h style={{position:"absolute", top:"65%"}}> tax </h>
-        <h style={{position:"absolute", top:"70%"}}> discount </h>
-        <h style={{position:"absolute", top:"75%"}}> estimated total </h>
+        <h style={{position:"relative", top:"10px",width:"200px", left:"10%",height:"10px"}}> subtotal </h>
+        <h style={{position:"relative", top:"30px",width:"200px",height:"10px"}}> tax </h>
+        <h style={{position:"relative", top:"50px",width:"200px" , left:"-10%",height:"10px"}}> discount </h>
+        <h style={{position:"relative", top:"70px",width:"200px",left:"-25%",height:"10px"}}> estimated total </h>
         
         <button type="submit" className='gotocheckout' style={{position:"absolute", top:"90%", color:"white",width:"400px"}}>Continue to check out  </button>
 
