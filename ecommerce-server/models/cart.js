c830ded0c747bcc12c8b1dd5058a2f7356c1b3ee
fanliushuai <f.liushuai@wustl.edu>
2024-08-05 14:55:49 -0700

    hello

--- ecommerce-server/models/cart.js
+++ ecommerce-server/models/cart.js
@@ -35,27 +35,7 @@ const cartSchema = new mongoose.Schema({
 
 });
 
-// userSchema.pre('save', async function (next) {
-//   try {
-//     if (!this.isModified('password')) {
-//       return next();
-//     }
-//     let hashedPassword = await bcrypt.hash(this.password, 10);
-//     this.password = hashedPassword;
-//     return next();
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
 
-// userSchema.methods.comparePassword = async function (candidatePassword, next) {
-//   try {
-//     let isMatched = await bcrypt.compare(candidatePassword, this.password);
-//     return isMatched;
-//   } catch (err) {
-//     return next(err);
-//   }
-// };
 
 const Cart = mongoose.model('Cart', cartSchema);
 
